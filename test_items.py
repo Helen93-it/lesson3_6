b553deff45af07bb78421c48e78b1657ee43b133
Helen Chirko <helen.chirko@gmail.com>
2022-04-06 22:35:00 +0300

    confest and tests

---
+++ test_items.py
@@ -0,0 +1,6 @@
+import pytest
+import time
+def test_find_button(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(5)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket")
